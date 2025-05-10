52c9edc2fc1095561f1ff22b9008e32e94ba2c7b
penewold <69643920+penewold@users.noreply.github.com>
2025-05-11 01:32:34 +0200

    succesful implementation :D

--- Overlay/src/main.cpp
+++ Overlay/src/main.cpp
@@ -29,7 +29,9 @@ ImColor healthTextColor = ImColor(0.1f, 0.9f, 0.1f);
 bool doTeamCheck = true;
 bool doHealthCheck = true;
 
-// Start do rendering rewrite
+void exitApp() {
+	PostQuitMessage(0);
+}
 
 INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
 
@@ -40,58 +42,7 @@ INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
 	screenDim.x = GetSystemMetrics(SM_CXSCREEN);
 	screenDim.y = GetSystemMetrics(SM_CYSCREEN);
 
-	WNDCLASSEXW wc = makeWindowClass(L"Overlay Class", instance);
-	RegisterClassExW(&wc);
-	HWND window = makeWindow(wc, instance, screenDim.x, screenDim.y, L"Overlay");
-	SetLayeredWindowAttributes(window, RGB(0, 0, 0), BYTE(255), LWA_ALPHA);
-	adjustWindowRect(window);
-	DXGI_SWAP_CHAIN_DESC sd = makeSwapChainDesc(window, 60U);
-
-	constexpr D3D_FEATURE_LEVEL levels[2]{
-		D3D_FEATURE_LEVEL_11_0,
-		D3D_FEATURE_LEVEL_10_0
-	};
-
-	ID3D11Device* device{ nullptr };
-	ID3D11DeviceContext* device_context{ nullptr };
-	IDXGISwapChain* swap_chain{ nullptr };
-	ID3D11RenderTargetView* render_target_view{ nullptr };
-	D3D_FEATURE_LEVEL level{};
-
-	D3D11CreateDeviceAndSwapChain(
-		nullptr,
-		D3D_DRIVER_TYPE_HARDWARE,
-		nullptr,
-		0U,
-		levels,
-		2U,
-		D3D11_SDK_VERSION,
-		&sd,
-		&swap_chain,
-		&device,
-		&level,
-		&device_context
-	);
-
-	ID3D11Texture2D* back_buffer{ nullptr };
-	swap_chain->GetBuffer(0U, IID_PPV_ARGS(&back_buffer));
-
-	if (back_buffer) {
-		device->CreateRenderTargetView(back_buffer, nullptr, &render_target_view);
-		back_buffer->Release();
-	}
-	else {
-		return 1;
-	}
-
-	ShowWindow(window, cmd_show);
-	UpdateWindow(window);
-
-	ImGui::CreateContext();
-	ImGui::StyleColorsClassic();
-
-	ImGui_ImplWin32_Init(window);
-	ImGui_ImplDX11_Init(device, device_context);
+	Drawer drawer(screenDim.x, screenDim.y, 60, instance, cmd_show);
 
 	memify mem("cs2.exe");
 
@@ -100,28 +51,8 @@ INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
 	bool running = true;
 
 	while (running) {
-		MSG msg;
-		while (PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE)) {
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
+		drawer.initFrame(exitApp);
 
-			if (msg.message == WM_QUIT) {
-				running = false;
-			}
-		}
-
-		if (!running) {
-			break;
-		}
-
-		ImGui_ImplDX11_NewFrame();
-		ImGui_ImplWin32_NewFrame();
-
-		ImGui::NewFrame();
-
-		ImDrawList* s = ImGui::GetBackgroundDrawList();
-		
-		
 		Matrix4 viewMatrix = mem.Read<Matrix4>(client + dwViewMatrix);
 		uintptr_t entityList = mem.Read<uintptr_t>(client + dwEntityList);
 		uintptr_t listEntry = mem.Read<uintptr_t>(entityList + 0x10);
@@ -172,49 +103,13 @@ INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
 			ImColor rainbowColor;
 			int rainbowTime = 2000;
 			rainbowColor = rainbowColor.HSV((GetTickCount() % rainbowTime) / (float)rainbowTime, .8f, .9f);
-			drawBox(s, topScreenPos.x - width, topScreenPos.y, bottomScreenPos.x + width, bottomScreenPos.y, 2.f, rainbowColor);
+			drawer.drawBox(topScreenPos.x - width, topScreenPos.y, bottomScreenPos.x + width, bottomScreenPos.y, rainbowColor, 2.f);
 			float healthbarPos = lerp(topScreenPos.x - width, bottomScreenPos.x + width, health / 100.f);
-			drawBoxFilled(s, topScreenPos.x - width, topScreenPos.y - 7.f, healthbarPos, topScreenPos.y - 3.f, 0.f, boxColor);
-			
-			//drawText(s, bottomScreenPos.x, bottomScreenPos.y, (int)distance(localPlayerPos, bottomPos));
+			drawer.drawBoxFilled(topScreenPos.x - width, topScreenPos.y - 7.f, healthbarPos, topScreenPos.y - 3.f, boxColor, 0.f);
 			
 		}
 	
-
-		ImGui::Render();
-
-		constexpr float color[4]{ 0.f, 0.f, 0.f, 0.f };
-		device_context->OMSetRenderTargets(1U, &render_target_view, nullptr);
-		device_context->ClearRenderTargetView(render_target_view, color);
-
-		ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
-
-		swap_chain->Present(1U, 0U); // set 1st arg to 0 to turn off vsync
+		drawer.drawFrame();
 	}
-
-	ImGui_ImplDX11_Shutdown();
-	ImGui_ImplWin32_Shutdown();
-
-	ImGui::DestroyContext();
-
-	if (swap_chain) {
-		swap_chain->Release();
-	}
-
-	if (device_context) {
-		device_context->Release();
-	}
-
-	if (device) {
-		device->Release();
-	}
-
-	if (render_target_view) {
-		render_target_view->Release();
-	}
-
-	DestroyWindow(window);
-	UnregisterClassW(wc.lpszClassName, wc.hInstance);
-
 	return 0;
 }
